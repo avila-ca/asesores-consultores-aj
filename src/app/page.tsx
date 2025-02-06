9ec35969f66a66e4481e5e359b6e0e1f08165d63
avila-ca <avilacalaf@gmail.com>
2025-02-06 20:02:22 +0100

    feature: navbar icons and page rutes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { roboto } from './ui/fonts';
 import styles from './ui/home.module.css';
 import clsx from 'clsx';
+import NavLinks from './ui/navLinks';
 
 export default function Home() {
   return (
@@ -10,10 +11,11 @@ export default function Home() {
 
           <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-30">
             <p className="text-white text-4xl">Asesores Consultores A.J.</p>
-            <nav>
-              
-            </nav>
+           
           </div>
+          <nav className='flex gap-2'>
+              <NavLinks/>
+            </nav>
           <button className={`${roboto.className} antialiased ${styles.buttonShape}`}>Presiona</button>
 
         </main>
--- src/app/ui/navLinks.tsx
+++ src/app/ui/navLinks.tsx
@@ -1,41 +1,46 @@
+'use client';
+
 import { usePathname } from "next/navigation";
-import * as React from 'react';
 import Link from "next/link";
 import clsx from "clsx";
-declare const HomeIcon: React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>;
-
-
+import {
+    HomeIcon,
+    ScaleIcon,
+    NewspaperIcon,
+    InformationCircleIcon,
+    PhoneIcon,
+  } from '@heroicons/react/24/outline';
 const links = [
-    {name: 'Home', href:'/inicio', icon: HomeIcon},
-    {name: 'Services', href:'/servicios', },
-    {name: 'News', href:'/noticias', },
-    {name: 'About', href:'/sobre-nosotros', },
-    {name: 'Contact', href:'/contacto', },
+    { name: 'Home', href: '/', icon: HomeIcon },
+    { name: 'Services', href: '/servicios', icon:ScaleIcon},
+    { name: 'News', href: '/noticias', icon:NewspaperIcon},
+    { name: 'About', href: '/sobre-nosotros', icon: InformationCircleIcon},
+    { name: 'Contact', href: '/contacto', icon: PhoneIcon},
 ]
 
 export default function NavLinks() {
     const pathname = usePathname();
 
-    return(
+    return (
         <>
-            {links.map((link)=>{
+            {links.map((link) => {
 
                 const LinkIcon = link.icon;
-                return(
+                return (
                     <Link
                         key={link.name}
                         href={link.href}
-                        className={ clsx(
+                        className={clsx(
                             'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
                             {
                                 'bg-sky-100 text-blue-600': pathname === link.href,
                             }
                         )}
-                        >
-                            <LinkIcon />
-                            <p className="hidden md:block">{ link.name }</p>
-                        </Link>
-                )            
+                    >
+                        {LinkIcon && <LinkIcon className="h-5 w-5" />}
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>
+                )
             })}
         </>
     )
